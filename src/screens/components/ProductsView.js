48c94f1747522b02025f3afae7549d9ab50885d0
gdoganieri <277168@studenti.unimore.it>
2021-06-23 17:17:18 +0200

    search in product list

--- src/screens/components/ProductsView.js
+++ src/screens/components/ProductsView.js
@@ -5,12 +5,14 @@ import {BackHandler} from "react-native";
 import {showAlert} from "../../Utils";
 import MapView from 'react-native-maps';
 import {ProductCard} from './styles';
+import {SearchBar} from 'react-native-elements';
 
 export default class ProductsView extends Component {
     constructor(props) {
     super(props);
     this.state={
         products:[],
+        search: '',
     }
   }
 
@@ -54,15 +56,60 @@ export default class ProductsView extends Component {
     </TouchableHighlight>
   );
 
-  render() {
-    return (
-      <View>
-        <FlatList
-          data={this.state.products}
-          renderItem={this.renderProduct}
-          keyExtractor={item => `${item.id}`}
-        />
-      </View>
+  updateSearch = (search) => {
+        let match = [];
+        let substring;
+        this.setState({search});
+        this.state.options.map((option) => {
+            substring = option.name.substring(0, search.length);
+
+            if (substring === search.toLowerCase()) {
+                match.push(option);
+                console.log(match);
+            }
+
+        });
+        this.setState({matches: match});
+    };
+
+    render() {
+        const {search} = this.state;
+        return (
+            <View>
+                <SearchBar
+                    containerStyle={{
+                        backgroundColor: 'transparent',
+                        borderBottomColor: 'transparent',
+                        borderTopColor: 'transparent',
+
+
+                    }}
+                    inputContainerStyle={{
+                        backgroundColor: '#EDEDED',
+                    }}
+                    inputStyle={{
+                        backgroundColor: '#EDEDED',
+                        borderRadius: 10,
+                        color: 'black',
+                    }}
+
+                    clearIcon
+                    //lightTheme
+                    round
+                    searchIcon
+
+                    placeholder="Search..."
+                    onChangeText={this.updateSearch}
+                    value={search}
+                    // onSubmitEditing={()}
+                    // autoCapitalize={}
+                />
+                <FlatList
+                  data={this.state.products}
+                  renderItem={this.renderProduct}
+                  keyExtractor={item => `${item.id}`}
+                />
+            </View>
     );
   }
 }
